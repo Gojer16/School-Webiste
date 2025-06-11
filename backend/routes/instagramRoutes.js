7f630ce66347ff600b308e5625573bac704cfff4
Gojer <gojer@naver.com>
2025-06-11 07:49:04 -0400

    Fixing...

--- backend/routes/instagramRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getInstagramFeed, refreshAccessToken } = require('../controllers/instagramController');
-
-router.get('/feed', getInstagramFeed);
-router.get('/refresh-token', refreshAccessToken);
-
-module.exports = router;
